9907f3de3007be6385023ae548739fa1691d38ff
MathisPct <math712b@gmail.com>
2022-11-19 11:44:45 +0100

    feat: add mongomapper composite and register to it

---
+++ MongoDBPoc/Db/Mongo/MongoMapperInitializer.cs
@@ -0,0 +1,19 @@
+﻿using MongoDBPoc.Db.Domain;
+using MongoDBPoc.Features.Category;
+
+namespace MongoDBPoc.Db;
+
+public class MongoMapperInitializer
+{
+    public static void Initialize()
+    {
+        MongoMapperAggregate mongoMapperAggregate = new MongoMapperAggregate();
+        
+        CategoryMap categoryMap = new CategoryMap();
+        VideoMap videoMap = new VideoMap();
+        mongoMapperAggregate.Add(categoryMap);
+        mongoMapperAggregate.Add(videoMap);
+        
+        mongoMapperAggregate.Map();
+    }
+}
