712abb062710188c40a9e780affefbe6b36fe8ea
MathisPct <math712b@gmail.com>
2022-11-26 11:30:51 +0100

    WIP: add dockerfile to facilitate the test (#4)

---
+++ DbTest/MockData.cs
@@ -0,0 +1,57 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDBPoc.Features.Category;
+using MongoDBPoc.Features.Video;
+
+namespace DbTest;
+
+public class MockData
+{
+    public static void InitData(DbFixture fixture)
+    {
+        fixture.DbContext.Video().InsertMany(new List<Video>()
+        {
+            new Video()
+            {
+                Id = ObjectId.GenerateNewId().ToString(),
+                Category = new List<Category>()
+                {
+                    new Category()
+                    {
+                        Id = ObjectId.GenerateNewId().ToString(),
+                        Name = "Jeux vidéos"
+                    },
+                    new Category()
+                    {
+                        Id = ObjectId.GenerateNewId().ToString(),
+                        Name = "Fun"
+                    }
+                },
+                Name = "Mario Kart 8",
+                Description = "Description",
+                YoutubeurId = "Youtubeur 1"
+            },
+            new Video()
+            {
+                Id = ObjectId.GenerateNewId().ToString(),
+                Category = new List<Category>()
+                {
+                    new Category()
+                    {
+                        Id = ObjectId.GenerateNewId().ToString(),
+                        Name = "Jeux vidéos"
+                    },
+                    new Category()
+                    {
+                        Id = ObjectId.GenerateNewId().ToString(),
+                        Name = "Jeux de tir"
+                    }
+                },
+                Name = "Splatoon 2",
+                Description = "Description",
+                YoutubeurId = "Youtubeur 1"
+            }
+        }   
+        );
+    }
+}
